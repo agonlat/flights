66924b290e600813bfcbfb03302cb75de35101b0
agonlat <95769510+agonlat@users.noreply.github.com>
2024-02-05 14:04:22 +0100

    Update Utility.java

--- src/main/java/com/example/fluganzeigetafel/Orders/Utilis/Utility.java
+++ src/main/java/com/example/fluganzeigetafel/Orders/Utilis/Utility.java
@@ -3,8 +3,16 @@ package com.example.fluganzeigetafel.Orders.Utilis;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
-
+/**
+ * The Utility class provides utility methods for handling common tasks.
+ */
 public class Utility {
+     /**
+     * Checks if the text fields in the given list are empty.
+     *
+     * @param nodes The list of nodes to check.
+     * @return true if any of the text fields are empty, false otherwise.
+     */
     public static boolean fieldsAreEmpty(ObservableList<Node> nodes) {
         for (Node node : nodes) {
             if (node instanceof TextField) {

977eb1b38f9e0e8cf37609b193d59325a1a62d81
agonlat <95769510+agonlat@users.noreply.github.com>
2024-02-05 13:14:10 +0100

    Update StatisticsManager.java

--- src/main/java/com/example/fluganzeigetafel/Flights/Manager/StatisticsManager.java
+++ src/main/java/com/example/fluganzeigetafel/Flights/Manager/StatisticsManager.java
@@ -15,11 +15,20 @@ import javafx.stage.Stage;
 
 import java.util.Map;
 
+ /**
+  * This class is responsible for the statistical data of the flights.
+  */
+
 public class StatisticsManager {
 private Stage stage;
 private Image img;
 private GridPane pane;
 private Scene scene;
+
+    
+ /**
+  * This constructor creates the window and includes all neccecary operations for showing the stats of all Flights.
+  */
     public StatisticsManager() {
          stage = new Stage();
          img = new Image(getClass().getResourceAsStream("/Icons/info-circle-fill.png"));
